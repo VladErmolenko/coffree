6f13d5f6e1b2ad549bedde22a49859f3a34ab3de
VladErmolenko <vladislav.culiev@gmail.com>
2021-01-29 17:42:24 +0300

    Upload screenshots

--- app/src/main/java/com/example/roomapp/fragments/update/UpdateCafeFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/update/UpdateCafeFragment.kt
@@ -10,7 +10,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.roomapp.R
-//import com.example.roomapp.UpdateCafeFragmentArgs
 import com.example.roomapp.model.Cafe
 import com.example.roomapp.viewmodel.CafeViewModel
 import kotlinx.android.synthetic.main.fragment_update_cafe.*
@@ -100,88 +99,3 @@ class UpdateCafeFragment : Fragment() {
     }
 
 }
-
-/*
-
-    private val args by navArgs<UpdateFragmentArgs>()
-
-    private lateinit var mUserViewModel: UserViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_update, container, false)
-
-        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
-
-        view.updateEmail_et.setText(args.currentUser.email)
-        view.updatePassword_et.setText(args.currentUser.password)
-        view.updateFirstName_et.setText(args.currentUser.firstName)
-        view.updateLastName_et.setText(args.currentUser.lastName)
-        view.updatePhoneNumber_et.setText(args.currentUser.phoneNumber)
-
-        view.update_btn.setOnClickListener {
-            updateItem()
-        }
-
-        // Add menu
-        setHasOptionsMenu(true)
-
-        return view
-    }
-
-    private fun updateItem() {
-
-        val email = updateEmail_et.text.toString()
-        val password = updatePassword_et.text.toString()
-        val firstName = updateFirstName_et.text.toString()
-        val lastName = updateLastName_et.text.toString()
-        val phoneNumber = updatePhoneNumber_et.text.toString()
-
-        if (inputCheck(firstName, lastName, phoneNumber)) {
-            // Create User Object
-            val updatedUser = User(args.currentUser.id,email,password, firstName, lastName, phoneNumber)
-            // Update Current User
-            mUserViewModel.updateUser(updatedUser)
-            Toast.makeText(requireContext(), "Updated Successfully!", Toast.LENGTH_SHORT).show()
-            // Navigate Back
-            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
-        } else {
-            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT)
-                .show()
-        }
-    }
-
-    private fun inputCheck(firstName: String, lastName: String, phoneNumber: String): Boolean {
-        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && TextUtils.isEmpty(phoneNumber))
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.delete_menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_delete) {
-            deleteUser()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun deleteUser() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setPositiveButton("Yes") { _, _ ->
-            mUserViewModel.deleteUser(args.currentUser)
-            Toast.makeText(
-                requireContext(),
-                "Successfully removed: ${args.currentUser.firstName}",
-                Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
-        }
-        builder.setNegativeButton("No") { _, _ -> }
-        builder.setTitle("Delete ${args.currentUser.firstName}?")
-        builder.setMessage("Are you sure you want to delete ${args.currentUser.firstName}?")
-        builder.create().show()
-    }
- */
